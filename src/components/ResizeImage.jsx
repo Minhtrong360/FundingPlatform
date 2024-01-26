8c449a6bed12affbef30aa14922f7e295a31feb0
Minhtrong360 <nguyenminhtrong402@gmail.com>
2024-01-26 09:30:18 +0700

    fixed NewsPage

--- src/components/ResizeImage.jsx
+++ src/components/ResizeImage.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 const ImageCropper = ({ className, imageUrl, width, height, onClick }) => {
-  const [croppedImageUrl, setCroppedImageUrl] = useState("");
+  const [scaledImageUrl, setScaledImageUrl] = useState("");
 
   useEffect(() => {
     if (imageUrl) {
-      cropImage(imageUrl);
+      scaleImage(imageUrl);
     }
   }, [imageUrl]);
 
-  const cropImage = (src) => {
+  const scaleImage = (src) => {
     const image = new Image();
     image.crossOrigin = "anonymous"; // Đảm bảo cho phép CORS
     image.src = src;
@@ -19,42 +19,29 @@ const ImageCropper = ({ className, imageUrl, width, height, onClick }) => {
       canvas.height = height;
       const ctx = canvas.getContext("2d");
 
+      // Điền nền trắng
+      ctx.fillStyle = "#FFFFFF";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
       // Tính toán tỷ lệ thu nhỏ hoặc phóng to
       const scaleX = canvas.width / image.width;
       const scaleY = canvas.height / image.height;
       const scale = Math.min(scaleX, scaleY);
 
-      // Tính toán kích thước mới của ảnh sau khi thu nhỏ
-      const newWidth = image.width * scale;
-      const newHeight = image.height * scale;
-
       // Tính toán vị trí để căn giữa ảnh
-      const offsetX = (canvas.width - newWidth) / 2;
-      const offsetY = (canvas.height - newHeight) / 2;
+      const offsetX = (canvas.width - image.width * scale) / 2;
+      const offsetY = (canvas.height - image.height * scale) / 2;
 
-      // Áp dụng biến đổi để thu nhỏ ảnh
-      ctx.drawImage(image, offsetX, offsetY, newWidth, newHeight);
-
-      // Tạo một canvas mới để cắt ảnh
-      const croppedCanvas = document.createElement("canvas");
-      croppedCanvas.width = width;
-      croppedCanvas.height = height;
-      const croppedCtx = croppedCanvas.getContext("2d");
-
-      // Cắt ảnh từ canvas gốc
-      croppedCtx.drawImage(
-        canvas,
+      // Áp dụng biến đổi để zoom in hoặc zoom out ảnh
+      ctx.drawImage(
+        image,
         offsetX,
         offsetY,
-        newWidth,
-        newHeight,
-        0,
-        0,
-        width,
-        height
+        image.width * scale,
+        image.height * scale
       );
 
-      setCroppedImageUrl(croppedCanvas.toDataURL("image/jpeg"));
+      setScaledImageUrl(canvas.toDataURL("image/jpeg"));
     };
   };
 
@@ -63,8 +50,8 @@ const ImageCropper = ({ className, imageUrl, width, height, onClick }) => {
       <img
         onClick={onClick}
         className={className}
-        src={croppedImageUrl}
-        alt="Cropped"
+        src={scaledImageUrl}
+        alt="Zoomed"
         style={{ width: "100%", height: "100%" }}
       />
     </div>
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -11,7 +11,7 @@ function BlogPost({ articles }) {
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto mt-28">
       <Header />
       <section className="bg-white dark:bg-gray-900">
-        <div className="container px-6 py-10 mx-auto">
+        <div className="container sm:px-6 py-10 mx-auto">
           <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
             Just Raised
           </h1>
@@ -19,62 +19,64 @@ function BlogPost({ articles }) {
           {articles.map((article, index) => (
             <div
               key={index}
-              className="py-10 px-5 my-28 lg:-mx-6 lg:flex lg:items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:border-transparent hover:shadow-lg transition-all duration-300"
+              className="my-28 border border-gray-200 rounded-lg shadow hover:border-transparent hover:shadow-lg transition-all duration-300 "
             >
-              <img
-                className="mx-auto  w-[34rem] lg:mx-6 rounded-xl h-[20rem]"
-                src={article.image_link}
-                alt=""
-              />
-              {/* <ResizeImage
+              <div className="lg:-mx-6 lg:flex lg:items-center px-4 py-4 ">
+                <img
+                  className="object-cover mx-auto w-[34rem] lg:mx-6 rounded-xl h-[20rem]"
+                  src={article.image_link}
+                  alt=""
+                />
+                {/* <ResizeImage
                 imageUrl={article.image_link}
                 width={566}
                 height={352}
               /> */}
 
-              <div className="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6">
-                <p className="text-sm text-blue-500 uppercase">Fundraising</p>
+                <div className="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6">
+                  <p className="text-sm text-blue-500 uppercase">Fundraising</p>
 
-                <a
-                  href={article.link}
-                  className="block mt-4 text-3xl font-semibold text-gray-800 hover:underline dark:text-white"
-                >
-                  {article.title}
-                </a>
+                  <a
+                    href={article.link}
+                    className="block mt-4 text-3xl font-semibold text-gray-800 hover:underline dark:text-white"
+                  >
+                    {article.title}
+                  </a>
 
-                <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm line-clamp-5">
-                  {article.abstract}
-                </p>
+                  <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm line-clamp-5">
+                    {article.abstract}
+                  </p>
 
-                <a
-                  href={article.link}
-                  className="inline-block mt-2 text-blue-500 underline hover:text-blue-400"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Read more
-                </a>
+                  <a
+                    href={article.link}
+                    className="inline-block mt-2 text-blue-500 underline hover:text-blue-400"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Read more
+                  </a>
 
-                <div className="flex items-center mt-6">
-                  <img
-                    className="object-cover object-center w-10 h-10 rounded-full"
-                    src="https://w0.peakpx.com/wallpaper/432/249/HD-wallpaper-sprout-beauty-enviroment-natural-plant-seed-tree-thumbnail.jpg"
-                    alt="Author"
-                  />
+                  <div className="flex items-center mt-6">
+                    <img
+                      className="object-cover object-center w-10 h-10 rounded-full"
+                      src="https://w0.peakpx.com/wallpaper/432/249/HD-wallpaper-sprout-beauty-enviroment-natural-plant-seed-tree-thumbnail.jpg"
+                      alt="Author"
+                    />
 
-                  <div className="mx-4">
-                    {article.authors.map((author, index) => (
-                      <h1
-                        key={index}
-                        className="text-sm text-gray-700 dark:text-gray-200"
-                      >
-                        {author}
-                      </h1>
-                    ))}
+                    <div className="mx-4">
+                      {article.authors.map((author, index) => (
+                        <h1
+                          key={index}
+                          className="text-sm text-gray-700 dark:text-gray-200"
+                        >
+                          {author}
+                        </h1>
+                      ))}
 
-                    <p className="text-sm text-gray-500 dark:text-gray-400">
-                      Author
-                    </p>
+                      <p className="text-sm text-gray-500 dark:text-gray-400">
+                        Author
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -129,7 +131,6 @@ function News() {
 
     fetchArticles();
   }, [page, perPage]);
-  console.log("articles", articles);
 
   return (
     <>

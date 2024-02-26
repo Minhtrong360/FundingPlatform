1ac9740fb1f31d36d87362ede541d8ad61c2a0e6
Minhtrong360 <nguyenminhtrong402@gmail.com>
2024-02-26 14:53:50 +0700

    dam bao progress bar luon luon bat dau tu 0

--- src/pages/FinalcialPage/FinancialPage.jsx
+++ src/pages/FinalcialPage/FinancialPage.jsx
@@ -1,17 +1,45 @@
 import React, { useEffect, useState } from "react";
 import AlertMsg from "../../components/AlertMsg";
 import SideBar from "../../components/SideBar";
-import FinancialForm from "./FinancialForm";
 
-import AnnouncePage from "../../components/AnnouncePage";
-import LoadingButtonClick from "../../components/LoadingButtonClick";
 import Z from "../test03";
+import { useAuth } from "../../context/AuthContext";
+import { supabase } from "../../supabase";
 
 function FinancialPage({ subscribed }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
+  const { user } = useAuth();
+
+  const [currentUser, setCurrentUser] = useState([]);
+
+  useEffect(() => {
+    // Tạo một async function để lấy thông tin người dùng từ Supabase
+    async function fetchUserData() {
+      try {
+        // Thực hiện truy vấn để lấy thông tin người dùng theo id (điều này cần được thay đổi dựa trên cấu trúc dữ liệu của bạn trong Supabase)
+        const { data, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("id", user.id) // Thay "id" bằng trường id thực tế trong cơ sở dữ liệu của bạn
+          .single(); // Sử dụng .single() để lấy một bản ghi duy nhất
+
+        if (error) {
+          throw error;
+        }
+
+        // Cập nhật state userData với thông tin người dùng đã lấy được
+        if (data) {
+          setCurrentUser(data);
+        }
+      } catch (error) {}
+    }
+
+    // Gọi hàm fetchUserData khi component được mount
+    fetchUserData();
+  }, [user.id, user.created_at]); // Sử dụng user.id làm phần tử phụ thuộc để useEffect được gọi lại khi user.id thay đổi
 
   return (
     <div className=" bg-white darkBg antialiased !p-0">
@@ -23,7 +51,7 @@ function FinancialPage({ subscribed }) {
           {/* {subscribed ? ( */}
           <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg darkBorderGray">
             {/* <FinancialForm /> */}
-            <Z />
+            <Z currentUser={currentUser} setCurrentUser={setCurrentUser} />
           </div>
           {/* ) : (
             <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg darkBorderGray">

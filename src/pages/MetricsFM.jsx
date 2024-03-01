a6b4ceef3dff08ca2081f2c8137b5dda190d624a
TakegawaPham <35416129+TakegawaPham@users.noreply.github.com>
2024-03-01 16:18:14 +0700

    new dark

--- src/pages/MetricsFM.jsx
+++ src/pages/MetricsFM.jsx
@@ -18,7 +18,7 @@ const MetricsFM = () => {
         {cardData.map((card, index) => (
           <div
             key={index}
-            className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800"
+            className="flex flex-col bg-white border shadow-sm rounded-xl"
           >
             <div className="p-4 md:p-5">
               <div className="flex items-center gap-x-2">

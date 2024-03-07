06cc0b3cc190cfd3a67eca25f505020055213ffc
TakegawaPham <35416129+TakegawaPham@users.noreply.github.com>
2024-03-07 18:02:20 +0700

    Customer xong

--- src/pages/FinalcialPage/FinancialPage.jsx
+++ src/pages/FinalcialPage/FinancialPage.jsx
@@ -4,7 +4,6 @@ import SideBar from "../../components/SideBar";
 import { useAuth } from "../../context/AuthContext";
 import { supabase } from "../../supabase";
 import FinancialForm from "./FinancialForm";
-import Z from "../test03";
 
 function FinancialPage({ subscribed }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -15,31 +14,31 @@ function FinancialPage({ subscribed }) {
 
   const [currentUser, setCurrentUser] = useState([]);
 
-  // useEffect(() => {
-  // //   // Tạo một async function để lấy thông tin người dùng từ Supabase
-  // //   async function fetchUserData() {
-  // //     try {
-  // //       // Thực hiện truy vấn để lấy thông tin người dùng theo id (điều này cần được thay đổi dựa trên cấu trúc dữ liệu của bạn trong Supabase)
-  // //       const { data, error } = await supabase
-  // //         .from("users")
-  // //         .select("*")
-  // //         .eq("id", user?.id) // Thay "id" bằng trường id thực tế trong cơ sở dữ liệu của bạn
-  // //         .single(); // Sử dụng .single() để lấy một bản ghi duy nhất
+  useEffect(() => {
+    // Tạo một async function để lấy thông tin người dùng từ Supabase
+    async function fetchUserData() {
+      try {
+        // Thực hiện truy vấn để lấy thông tin người dùng theo id (điều này cần được thay đổi dựa trên cấu trúc dữ liệu của bạn trong Supabase)
+        const { data, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("id", user?.id) // Thay "id" bằng trường id thực tế trong cơ sở dữ liệu của bạn
+          .single(); // Sử dụng .single() để lấy một bản ghi duy nhất
 
-  // //       if (error) {
-  // //         throw error;
-  // //       }
+        if (error) {
+          throw error;
+        }
 
-  // //       // Cập nhật state userData với thông tin người dùng đã lấy được
-  // //       if (data) {
-  // //         setCurrentUser(data);
-  // //       }
-  // //     } catch (error) {}
-  // //   }
+        // Cập nhật state userData với thông tin người dùng đã lấy được
+        if (data) {
+          setCurrentUser(data);
+        }
+      } catch (error) {}
+    }
 
-  // //   // Gọi hàm fetchUserData khi component được mount
-  // //   fetchUserData();
-  // // }, [user?.id]); // Sử dụng user.id làm phần tử phụ thuộc để useEffect được gọi lại khi user.id thay đổi
+    // Gọi hàm fetchUserData khi component được mount
+    fetchUserData();
+  }, [user?.id]); // Sử dụng user.id làm phần tử phụ thuộc để useEffect được gọi lại khi user.id thay đổi
 
 // 1
   
@@ -51,26 +50,10 @@ function FinancialPage({ subscribed }) {
         <div className="p-4 ml-24" onClick={() => setIsSidebarOpen(false)}>
           {/* {subscribed ? ( */}
           <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg darkBorderGray">
-<<<<<<< HEAD
-<<<<<<< HEAD
-            {/* <FinancialForm
-              currentUser={currentUser}
-              setCurrentUser={setCurrentUser}
-            /> */}
-            
-=======
-             {/* {subscribed ? ( */}
-=======
->>>>>>> 218236d2dd6d90f6dc4da0a23d5ab13c793122c5
             <FinancialForm
               currentUser={currentUser}
               setCurrentUser={setCurrentUser}
             />
-<<<<<<< HEAD
-             {/* {subscribed ? ( */}
->>>>>>> 8b11a19043451961e92db66a5e21c1e1bab4dac8
-=======
->>>>>>> 218236d2dd6d90f6dc4da0a23d5ab13c793122c5
           </div>
           {/* ) : (
             <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg darkBorderGray">

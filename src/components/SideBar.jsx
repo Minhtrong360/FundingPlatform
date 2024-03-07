7d69ec73fd77d7aecfd3a77f2dfbb7d35225de17
longPVD01 <long.phan@beekrowd.com>
2024-03-07 09:08:19 +0700

    06-03-24

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -24,7 +24,7 @@ function SideBar({ toggleSidebar, isSidebarOpen }) {
       id="default-sidebar"
       className={`fixed top-0 left-0 z-50 h-screen transition-all duration-300 ease-in-out transform ${
         isSidebarOpen ? "w-auto" : "w-24 "
-      } group hover:w-64 bg-gray-50 dark:bg-blue-900`}
+      } group hover:w-64 bg-gray-50 `}
       aria-label="Sidebar"
     >
       <style>
@@ -50,56 +50,56 @@ function SideBar({ toggleSidebar, isSidebarOpen }) {
           }
         `}
       </style>
-      <div className="mx-auto flex flex-col items-start h-full px-3 py-4 space-y-2 overflow-y-auto">
+      <div className="mt-16 mx-auto flex flex-col items-start h-full px-3 py-4 space-y-2 overflow-y-auto">
         <button
-          className="w-full p-2 group flex items-center rounded-lg font-semibold text-xl text-blue-600 sidebar-button"
+          className="mt-2  w-full p-2 group flex items-center rounded-lg text-sm  sidebar-button"
           onClick={() => navigate("/")}
         >
-          <HomeOutlinedIcon fontSize="large" />
-          <span className="sidebar-text">BeeKrowd</span>
+          <img width="18" height="18" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>
+          <span className="sidebar-text">Home</span>
         </button>
         <button
-          className={`w-full p-2 group flex items-center rounded-lg font-medium ${
+          className={`mt-2 mb-2 w-full p-2 group flex items-center rounded-lg text-sm ${
             selectedItem?.includes("/user-info")
               ? "bg-gray-300 "
               : "text-gray-900 darkTextWhite"
           } sidebar-button`}
           onClick={() => handleItemClick("/user-info")}
         >
-          <AccountCircleOutlinedIcon fontSize="large" />
+          <img width="18" height="18" src="https://img.icons8.com/ios/50/user--v1.png" alt="user--v1"/>
           <span className="sidebar-text">User Settings</span>
         </button>
         <button
-          className={`w-full p-2 group flex items-center rounded-lg font-medium ${
+          className={`mt-2 mb-2 w-full p-2 group flex items-center rounded-lg text-sm ${
             selectedItem?.includes("/dashboard")
               ? "bg-gray-300 "
               : "text-gray-900 darkTextWhite"
           } sidebar-button`}
           onClick={() => handleItemClick("/dashboard")}
         >
-          <SpaceDashboardOutlinedIcon fontSize="large" />
+         <img width="18" height="18" src="https://img.icons8.com/ios/50/control-panel--v2.png" alt="control-panel--v2"/>
           <span className="sidebar-text">Dashboard</span>
         </button>
         <button
-          className={`w-full p-2 group flex items-center rounded-lg font-medium ${
+          className={`mt-2 mb-2 w-full p-2 group flex items-center rounded-lg text-sm ${
             selectedItem?.includes("/financials")
               ? "bg-gray-300 "
               : "text-gray-900 darkTextWhite"
           } sidebar-button`}
           onClick={() => handleItemClick("/financials")}
         >
-          <CalculateOutlinedIcon fontSize="large" />
+          <img width="18" height="18" src="https://img.icons8.com/ios/50/investment.png" alt="investment"/>
           <span className="sidebar-text">Financials</span>
         </button>
         <button
-          className={`w-full p-2 group flex items-center rounded-lg font-medium  ${
+          className={`mt-2 mb-2 w-full p-2 group flex items-center rounded-lg text-sm  ${
             selectedItem?.includes("/founder")
               ? "bg-gray-300"
               : "text-gray-900 darkTextWhite"
           } sidebar-button`}
           onClick={() => handleItemClick("/founder")}
         >
-          <FolderCopyOutlinedIcon fontSize="large" />
+          <img width="18" height="18" src="https://img.icons8.com/ios/50/folder-invoices--v1.png" alt="folder-invoices--v1"/>
           <span className="sidebar-text">Projects</span>
         </button>
       </div>

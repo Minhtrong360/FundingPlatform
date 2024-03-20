e76b1aa6cf97d25bd021d6ee6350698be86f5aed
Minhtrong360 <nguyenminhtrong402@gmail.com>
2024-03-20 16:29:11 +0700

    Redux done 3

--- src/pages/FinalcialPage/Components/CustomerChart.jsx
+++ src/pages/FinalcialPage/Components/CustomerChart.jsx
@@ -2,26 +2,68 @@ import React from "react";
 import Chart from "react-apexcharts";
 import { Col, Card } from "antd";
 
-const CustomChart = ({ chartOptions, data, title }) => (
-  <Col span={24} md={12} className="md:mt-0 mt-5">
-    <Card title={title}>
-      <Chart
-        options={{
-          ...chartOptions,
-          chart: { ...chartOptions.chart, id: "total-revenue-chart" },
-          yaxis: { title: { text: "Total Revenue ($)" } },
-        }}
-        series={[
-          {
-            name: "Total Revenue",
-            data: data.map((value) => parseFloat(value.toFixed(2))),
-          },
-        ]}
-        type="area"
-        height={300}
-      />
-    </Card>
-  </Col>
-);
+const CustomChart = ({
+  numberOfMonths,
+
+  id,
+  yaxisTitle,
+  seriesTitle,
+  RenderData,
+  title,
+}) => {
+  const chartOptions = {
+    chart: { id: "profit-and-loss-chart", type: "line", height: 350 },
+    xaxis: {
+      categories: Array.from({ length: numberOfMonths }, (_, i) => `${i + 1}`),
+      title: {
+        text: "Month",
+        style: {
+          fontFamily: "Inter, sans-serif",
+          fontWeight: "600",
+        },
+      },
+      labels: {
+        rotate: -45,
+        rotateAlways: false,
+        hideOverlappingLabels: true,
+        trim: true,
+        minHeight: 100,
+        style: {
+          cssClass: "apexcharts-xaxis-label",
+        },
+      },
+    },
+    yaxis: { title: { text: "Amount ($)" } },
+    stroke: { curve: "smooth" },
+    legend: { position: "top" },
+    tooltip: {
+      enabled: true,
+      y: {
+        formatter: (val) => `$${val?.toFixed(2)}`,
+      },
+    },
+  };
+  return (
+    <Col span={24} md={12} className="md:mt-0 mt-5">
+      <Card title={title}>
+        <Chart
+          options={{
+            ...chartOptions,
+            chart: { ...chartOptions.chart, id: id },
+            yaxis: { title: { text: yaxisTitle } },
+          }}
+          series={[
+            {
+              name: seriesTitle,
+              data: RenderData.map((value) => parseFloat(value.toFixed(2))),
+            },
+          ]}
+          type="area"
+          height={300}
+        />
+      </Card>
+    </Col>
+  );
+};
 
 export default CustomChart;

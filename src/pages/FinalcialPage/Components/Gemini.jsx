51ce41b641fe83e5986a5d2b1b005335a36c9a59
Minhtrong360 <nguyenminhtrong402@gmail.com>
2024-03-20 09:19:07 +0700

    nothing new

--- src/pages/FinalcialPage/Components/Gemini.jsx
+++ src/pages/FinalcialPage/Components/Gemini.jsx
@@ -223,11 +223,12 @@ const Gemini = ({
       const maxPrompt = 20;
       // Thực hiện truy vấn để lấy thông tin người dùng theo id (điều này cần được thay đổi dựa trên cấu trúc dữ liệu của bạn trong Supabase)
       const currentPrompt = currentUser.financePromptNumber - 1;
+      console.log("currentPrompt", currentPrompt);
       if (currentPrompt <= 0) {
         toast.warning("Prompt per hour limited. Let return after an hour.");
         return;
       } else {
-        if (currentPrompt === maxPrompt - 1) {
+        if (currentPrompt <= maxPrompt - 1) {
           console.log("7.5");
           await supabase
             .from("users")

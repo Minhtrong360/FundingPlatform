b2dee4b2320e72546303861040a741e0022e7050
Minhtrong360 <nguyenminhtrong402@gmail.com>
2024-04-02 17:43:24 +0700

    ...

--- src/pages/FounderGitbook/Components/Introduction.jsx
+++ src/pages/FounderGitbook/Components/Introduction.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Introduction() {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        In the bustling heart of Silicon Valley, nestled among the towering tech
+        giants, lies a beacon of innovation and sustainability: VoltDrive
+        Electric. Founded by visionary engineer, Emily Rodriguez, VoltDrive is
+        not just an electric car company; it's a revolution on wheels.
+      </p>
+      <br />
+      <p>
+        At VoltDrive, every day begins with a dedication to a greener, cleaner
+        future. Their state-of-the-art manufacturing facility, powered entirely
+        by renewable energy, is a testament to their commitment to
+        sustainability. The sleek, modern architecture of the building mirrors
+        the company's ethos: merging cutting-edge technology with eco-conscious
+        design.
+      </p>
+    </div>
+  );
 }
 
 export default Introduction;

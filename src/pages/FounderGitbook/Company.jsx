4eb3da66a74a83d958afdbe6336fe4065b0ee444
TakegawaPham <35416129+TakegawaPham@users.noreply.github.com>
2024-04-03 18:05:22 +0700

    Thêm scroll bar

--- src/pages/FounderGitbook/Company.jsx
+++ src/pages/FounderGitbook/Company.jsx
@@ -83,15 +83,15 @@ function Company({
   }, []);
 
   return (
-    <>
+    <> <div className="max-w-xl mx-auto overflow-auto h-screen">
       <div className="max-w-[85rem] px-3 py-20 sm:px-6 lg:px-8 lg:py-14 mx-auto lg:border-r-2 border-r-0">
         <AlertMsg />
+       
+
         <div className="max-w-xl mx-auto">
-          <div className="text-center">
-            <h1 className="text-3xl font-semibold text-gray-800 sm:text-4xl darkTextWhite">
-              Company info
-            </h1>
-            <p className="mt-1 text-gray-600 darkTextGray">
+          <div className="text-left">
+          
+            <p className="mt-1 text-gray-800 font-semibold darkTextGray">
               Please fill basic information about your company.
             </p>
           </div>
@@ -162,7 +162,7 @@ function Company({
                 </div>
 
                   <SelectField
-                    label="Annual revenue last year"
+                    label="Annual revenue range"
                     id="revenueStatus"
                     name="revenueStatus"
                     value={formData.revenueStatus}
@@ -315,7 +315,8 @@ function Company({
             </form>
           </div>
         </div>
-      </div>
+        </div>
+        </div>
     </>
   );
 }

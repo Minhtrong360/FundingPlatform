ae575e488ced9a4af5ce925ea58b17e79aad456f
TakegawaPham <35416129+TakegawaPham@users.noreply.github.com>
2024-04-10 15:15:53 +0700

    Commit 15.15pm

--- src/pages/FounderGitbook/Company.jsx
+++ src/pages/FounderGitbook/Company.jsx
@@ -201,8 +201,8 @@ function Company({
                   />
 
                   <InputField
-                    label="Key words"
-                    title="Key words should be separated by comma (,)"
+                    label="Keywords"
+                    title="Keywords should be separated by comma (,)"
                     id="keyWords"
                     name="keyWords"
                     value={formData.keyWords}
@@ -244,7 +244,7 @@ function Company({
 
                   <div>
                     <InputField
-                      label="Card Image link"
+                      label="Profile Image link"
                       id="card_url"
                       name="card_url"
                       value={formData.card_url}

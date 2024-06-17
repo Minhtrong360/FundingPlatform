234aacf2737cfc6a9d25e75933c8efde0e30e11c
Minhtrong360 <nguyenminhtrong402@gmail.com>
2024-06-17 17:38:05 +0700

    height chatBotView 500

--- src/pages/FinalcialPage/Components/CustomerSection.jsx
+++ src/pages/FinalcialPage/Components/CustomerSection.jsx
@@ -1555,7 +1555,7 @@ const CustomerSection = React.memo(
                 />
               </div>
               <div className="w-full xl:w-1/4 sm:p-4 p-0   ">
-                {/* <Flowise prompt={generatePromptC()} button={"Send"} /> */}
+                <Flowise prompt={generatePromptC()} button={"Send"} />
                 <button
                   className="bg-blue-600 text-white py-2 px-2 text-sm rounded-2xl mt-4 min-w-[6vw] "
                   style={{ bottom: "20px", right: "80px", position: "fixed" }}

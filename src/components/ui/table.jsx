dc5ead95346b6e9a6e4af8567dcc99d6a9ca2670
Minhtrong360 <nguyenminhtrong402@gmail.com>
2024-09-16 10:28:13 +0700

    fit table man hinh !p-4

--- src/components/ui/table.jsx
+++ src/components/ui/table.jsx
@@ -55,7 +55,7 @@ const TableHead = React.forwardRef(({ className, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
-      "h-12 xl:!px-6 !px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "h-12 !px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
@@ -66,10 +66,7 @@ TableHead.displayName = "TableHead";
 const TableCell = React.forwardRef(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn(
-      "xl:!p-6 !p-2 align-middle [&:has([role=checkbox])]:pr-0",
-      className
-    )}
+    className={cn("!p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
     {...props}
   />
 ));
